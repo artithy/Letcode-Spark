a7f39e88908929110bc08c1915b1d20047955ef0
Afrina Tithy <afrina.tithy@gmail.com>
2025-03-11 16:07:27 +0600

    isValidBrackets.js

---
+++ isValidBrackets.js
@@ -0,0 +1,28 @@
+function isValid(s) {
+    let stack = [];
+    for (var i = 0; i < s.length; i++) {
+        var char = s[i];
+        if (char == '(' || char == '{' || char == '[') {
+            stack.push(char);
+        } else {
+            if (stack.length == 0) {
+                return false;
+            }
+
+
+            let last = stack.pop()
+            if (
+                (char == ')' && last == '(') || (char == '}' && last == '{') || (char == ']' && last == '[')
+            ) {
+                continue;
+            } else {
+                return false;
+
+            }
+        }
+    }
+
+    return stack.length == 0;
+}
+console.log(isValid("()"))
+console.log(isValid("({)}"))
