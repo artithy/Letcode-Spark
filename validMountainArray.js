08e56b695ea6cfddd0c5a98913f0f386abeebdf2
Afrina Tithy <afrina.tithy@gmail.com>
2025-08-03 22:40:00 +0600

    validMountainArray.js

---
+++ validMountainArray.js
@@ -0,0 +1,25 @@
+var validMountainArray = function (arr) {
+    var i = 0, n;
+    n = arr.length;
+    if (n < 3) {
+        return false;
+    }
+
+    while (i + 1 < n && arr[i] < arr[i + 1]) {
+        i++;
+    }
+
+    if (i == 0 || i == n - 1) {
+        return false;
+    }
+
+    while (i + 1 < n && arr[i] > arr[i + 1]) {
+        i++;
+    }
+
+    if (i == n - 1) {
+        return true;
+    } else {
+        return false;
+    }
+};
