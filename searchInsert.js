fd31164b372c958b1a54a9a962f4c0922030ded0
Afrina Tithy <afrina.tithy@gmail.com>
2025-09-15 10:16:30 +0600

    searchInsert.js

---
+++ searchInsert.js
@@ -0,0 +1,15 @@
+var searchInsert = function (nums, target) {
+    var low = 0;
+    var high = nums.length - 1;
+    while (low <= high) {
+        var mid = Math.floor((low + high) / 2)
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            low = mid + 1;
+        } else if (nums[mid] > target) {
+            high = mid - 1;
+        }
+    }
+    return low;
+};
