85ac261758e9ae4dd35cfb34750b87998cbb8a93
Afrina Tithy <afrina.tithy@gmail.com>
2025-10-29 23:37:05 +0600

    missingNumber.c

---
+++ missingNumber.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int missingNumber(int nums[], int n)
+{
+    int expectedSum = n * (n + 1) / 2;
+    int actualSum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        actualSum += nums[i];
+    }
+
+    return expectedSum - actualSum;
+}
+
+int main()
+{
+    int nums[] = {3, 0, 1};
+    int n = sizeof(nums) / sizeof(nums[0]);
+
+    int missing = missingNumber(nums, n);
+    printf("Missing number is: %d\n", missing);
+
+    return 0;
+}
